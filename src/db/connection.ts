461f6ba4b986b00e986c986c6a16acda349ce1d9
eduardomtzchenoweth <eduardomtzchenoweth@gmail.com>
2024-10-15 11:00:45 -0700

    Change conection to localmongo

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,36 +1,28 @@
 import mongoose from 'mongoose';
 mongoose.Promise = global.Promise;
 
-
 class DBConnection {
   private static hasBeenConnected = false;
 
-
   private static buildURI(): string {
-    /*
     const MONGO_HOSTNAME = process.env.MONGO_HOSTNAME;
     const MONGO_PORT = process.env.MONGO_PORT;
     const MONGO_DB = process.env.MONGO_DB;
     const MONGO_USERNAME = process.env.MONGO_USERNAME;
-    const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
-    */
-   
-    return `${process.env.MONGO_URI}`;
+    const MONGO_PASSWORD = encodeURIComponent(process.env.MONGO_PASSWORD || '');
+
+    return `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=${MONGO_DB}`;
   }
-  
 
   public static async connect(): Promise<void> {
     if (!DBConnection.hasBeenConnected) {
       const uri = DBConnection.buildURI();
       const options = {
         useNewUrlParser: true,
-        // Mongoose will automatically build indexes defined in your schema when it connects.
-        autoIndex: true,
-        // Set to true to make Mongoose's default index build use
-        useCreateIndex: true,
-        // The maximum number of sockets the MongoDB driver will keep open for this connection.
-        poolSize: 10,
+        autoIndex: true,  
         useUnifiedTopology: true,
+        useCreateIndex: true, 
+        poolSize: 10,  
         useFindAndModify: false,
       };
 
@@ -38,8 +30,8 @@ class DBConnection {
         await mongoose.connect(uri, options);
         console.log('MongoDB is connected.');
         DBConnection.hasBeenConnected = true;
-      } catch(err) {
-        console.log(err);
+      } catch (err) {
+        console.log('Error connecting to MongoDB:', err);
       }
     }
   }
